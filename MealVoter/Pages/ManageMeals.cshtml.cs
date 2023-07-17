2ac995a1de3dfd6990c84f6d9b5c6c655e5bf098
Ben Sabatier (sabatierb) <bensab62@gmail.com>
2023-07-17 14:39:41 +0100

    Added Manage Meal Page

---
+++ MealVoter/Pages/ManageMeals.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MealVoter.Pages
+{
+    public class ManageMealsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
