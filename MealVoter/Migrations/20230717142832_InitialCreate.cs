5008ecf2860728bea4e3099ff01ee535f592af26
Ben Sabatier (sabatierb) <bensab62@gmail.com>
2023-07-17 15:34:28 +0100

    Migrations created for DB

---
+++ MealVoter/Migrations/20230717142832_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MealVoter.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meals", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meals");
+        }
+    }
+}
--- MealVoter/Pages/ManageMeals.cshtml.cs
+++ MealVoter/Pages/ManageMeals.cshtml.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MealVoter.Models;
+using MealVoter.Services;
 
 namespace MealVoter.Pages
 {
     public class ManageMealsModel : PageModel
     {
+        private readonly MealService _service;
+        public IList<Meal> MealList { get; set;} = default!;
+
+        public ManageMealsModel(MealService service)
+        {
+            _service = service;
+        }
         public void OnGet()
         {
+            MealList = _service.GetMeals();
         }
     }
 }
